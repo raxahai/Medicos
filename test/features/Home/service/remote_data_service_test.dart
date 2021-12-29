38ebd0b92a2c4dd7d5f97e3ea32a3c38012c1e17
Raza Haider <raxa.hai@gmail.com>
2021-12-29 21:07:14 +0500

    Fix(test): remote_data_test Uri issue

--- test/features/Home/service/remote_data_service_test.dart
+++ test/features/Home/service/remote_data_service_test.dart
@@ -7,11 +7,14 @@ import 'package:test_me/features/Home/service/remote_data_service.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
+class FakeUri extends Fake implements Uri {}
+
 void main() {
   late MockHttpClient mockHttpClient;
   late HomeRemoteDataSourceImplementation homeRemoteDataSourceImplementation;
 
   setUp(() {
+    registerFallbackValue(FakeUri());
     mockHttpClient = new MockHttpClient();
     homeRemoteDataSourceImplementation =
         new HomeRemoteDataSourceImplementation(client: mockHttpClient);
@@ -21,8 +24,7 @@ void main() {
     test(("should perform a GET request on a URL with application/json header"),
         () {
       when(() => mockHttpClient.get(
-            Uri.parse(// cant use any as null exception
-                "https://run.mocky.io/v3/30579b01-75f8-4c9f-ac83-c98b5cfe1d2e"),
+            any(),
             headers: any(named: "headers"),
           )).thenAnswer(
         (_) async => http.Response(
