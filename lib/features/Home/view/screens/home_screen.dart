fc8bc999a223dfbb75bc4d8cef2fda3c733d8d6f
Raza Haider <raxa.hai@gmail.com>
2021-12-31 03:58:18 +0500

    feat(home): Add view and view-model

---
+++ lib/features/Home/view/screens/home_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:test_me/core/widgets/app_bar_widget.dart';
+import 'package:test_me/features/Home/view_model/home_view_model.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  final HomeViewModel homeViewModel = Get.put(HomeViewModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Scaffold(
+        backgroundColor: Colors.white,
+        appBar: CustomAppBar(),
+        body: homeViewModel.isLoading.value
+            ? Center(child: CircularProgressIndicator())
+            : Container(
+                margin: EdgeInsets.only(left: 10, top: 20, right: 10),
+                child: Column(
+                  children: [
+                    userDetailWidget(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Medication:",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(fontSize: 20),
+                          )),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 4,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      height: 105,
+                      child: Column(
+                        children: [
+                          customKeyValue(
+                            key: "Name: ",
+                            value: "${homeViewModel.associatedDrug!.name}",
+                          ),
+                          customKeyValue(
+                              key: "Dose: ",
+                              value: "${homeViewModel.associatedDrug!.dose}"),
+                          customKeyValue(
+                              key: "Strength: ",
+                              value:
+                                  "${homeViewModel.associatedDrug!.strength}"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
+  }
+
+  Widget customKeyValue({required String key, required String value}) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: Row(
+        children: [
+          Text(
+            key,
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            value.isEmpty ? "N/A" : value,
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userDetailWidget() {
+    return Container(
+      height: 90.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 4,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+                child: Container(
+                    child: Image.asset(
+                  "assets/icons/user.png",
+                  width: 35,
+                  height: 35,
+                )),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Syed Raza Haider",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/Home/view_model/home_view_model.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
+import 'package:test_me/core/errors/failures.dart';
+import 'package:test_me/features/Home/model/associated_drug_model.dart';
+import 'package:test_me/features/Home/model/medical_record_model.dart';
+import 'package:test_me/features/Home/model/problems_model.dart';
+import 'package:test_me/features/Home/repository/home_repository.dart';
+import 'package:test_me/features/Home/service/remote_data_service.dart';
+import 'package:http/http.dart' as http;
+
+class HomeViewModel extends GetxController {
+  HomeRepository homeRepository = HomeRepositoryImpl(
+    remoteDataSource: HomeRemoteDataSourceImplementation(
+      client: http.Client(),
+    ),
+  );
+
+  Rx<bool> isLoading = false.obs;
+  AssociatedDrug? associatedDrug;
+
+  @override
+  void onInit() {
+    getFirstClassNameMedication();
+    super.onInit();
+  }
+
+  Future<void> getFirstClassNameMedication() async {
+    isLoading(true);
+    Either<Failure, MedicalRecordModel> call =
+        await homeRepository.getMedicalRecord();
+
+    MedicalRecordModel medicalRecordModel =
+        call.fold((l) => throw UnimplementedError, (r) => r);
+    List<Problems>? problems = medicalRecordModel.problems;
+
+    // Since list only had one object in each nested json so used
+    // so used this technique otherwise have used loop or recursion
+    associatedDrug = problems!.first.diabetes!.first.medications!.first
+        .medicationsClasses!.first.className!.first.associatedDrug!.first;
+    isLoading(false);
+  }
+}
