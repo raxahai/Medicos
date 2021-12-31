b049697988efb902a10ec485ef77a694cf457afe
Raza Haider <raxa.hai@gmail.com>
2021-12-31 13:12:14 +0500

    fix(image-load): compressed image

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    precacheImage(AssetImage("assets/icons/medicos.png"), context);
     return Sizer(
       builder: (context, orientation, deviceType) => GetMaterialApp(
         title: 'Flutter Demo',
